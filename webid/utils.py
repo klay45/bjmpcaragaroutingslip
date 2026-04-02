5014a1c6f668c571499e2b0350546947ad794f46
klay45 <126217816+klay45@users.noreply.github.com>
2026-04-02 08:33:16 +0800

    Update utils.py

--- webid/utils.py
+++ webid/utils.py
@@ -1,5 +1,5 @@
-import firebase_admin
-from firebase_admin import credentials
+#import firebase_admin
+#from firebase_admin import credentials
 
-cred = credentials.Certificate('C:/Users/epcc/epccid/push-notification-a806b-firebase-adminsdk-tn1ee-756c9c61ce.json')
-firebase_admin.initialize_app(cred)
+#cred = credentials.Certificate('C:/Users/epcc/epccid/push-notification-a806b-firebase-adminsdk-tn1ee-756c9c61ce.json')
+#firebase_admin.initialize_app(cred)
